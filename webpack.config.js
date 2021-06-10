f81edb07465a04e2121e1ae267dc2b110eee4f94
raratiru <tantiras@yandex.com>
2021-06-10 20:56:31 +0300

    Added MiniCssExtract and Postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,109 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: { "assets/index": "./src/index.js" },
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].js",
-    clean: true,
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "src/index.html", minify: true }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS using node-sass for vr-reset
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("node-sass"),
-              sassOptions: {
-                fiber: false,
+
+module.exports = (env) => {
+  return {
+    mode: env.production ? "production" : "development",
+    entry: { "assets/index": "./src/index.js" },
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name].js",
+      clean: true,
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "src/index.html", minify: true }),
+      new MiniCssExtractPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            env.production ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
               },
             },
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      "postcss-preset-env",
+                      {
+                        // Options
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
-        ],
-      },
-    ],
-  },
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            env.production ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      "postcss-preset-env",
+                      {
+                        // Options
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
+            // Compiles Sass to CSS using node-sass for vr-reset
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("node-sass"),
+                sassOptions: {
+                  fiber: false,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+        // `...`,
+        new CssMinimizerPlugin(),
+      ],
+    },
+  };
 };
